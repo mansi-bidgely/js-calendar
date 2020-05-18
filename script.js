454e7bac4535e683d9dc8dd9fd913e1c2dda60b5
Mansi Jain <mansijain3113@gmail.com>
2020-05-18 13:45:59 +0530

    review changes

--- script.js
+++ script.js
@@ -33,46 +33,59 @@ document.getElementById("thead-month").innerHTML = $dataHead;
 monthAndYear = document.getElementById("monthAndYear");
 showCalendar(currentMonth, currentYear);
 
-function next() {
-  currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-  currentMonth = (currentMonth + 1) % 12;
+
+function moveDate(para) {
+  if (para == "prev") {
+    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+  } else if (para == 'next') {
+    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+  }
   showCalendar(currentMonth, currentYear);
 }
 
-function previous() {
-  currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-  currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-  showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
+function change_month_year() {
   currentYear = parseInt(selectYear.value);
   currentMonth = parseInt(selectMonth.value);
+  document.getElementById("date-current").innerHTML = "";
+
   showCalendar(currentMonth, currentYear);
 }
+
 
 function showCalendar(month, year) {
   var firstDay = (new Date(year, month)).getDay();
-  tbl = document.getElementById("calendar-body");
-  tbl.innerHTML = "";
+  table = document.getElementById("calendar-body");
+  table.innerHTML = "";
   monthAndYear.innerHTML = months[month] + " " + year;
   selectYear.value = year;
   selectMonth.value = month;
+  var prevDate = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    0
+  ).getDate();
   // creating all cells
   var date = 1;
   for (var i = 0; i < 6; i++) {
     var row = document.createElement("tr");
     for (var j = 0; j < 7; j++) {
       if (i === 0 && j < firstDay) {
-        cell = document.createElement("td");
-        cellText = document.createTextNode("");
-        cell.appendChild(cellText);
+
+
+        var cell = document.createElement("td");
+
+
+        cell.innerHTML = "<span class='prev-date' disabled>" + (prevDate - firstDay + j + 1) + "</span>";
         row.appendChild(cell);
+
+
       } else if (date > daysInMonth(month, year)) {
         break;
       } else {
         cell = document.createElement("td");
-       
+
         cell.innerHTML = "<span>" + date + "</span>";
         if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
           cell.className = "date-picker selected";
@@ -81,15 +94,31 @@ function showCalendar(month, year) {
         date++;
       }
     }
-    tbl.appendChild(row);
+    table.appendChild(row);
   }
   var allTableCells = document.getElementsByTagName("td");
+
   for (var i = 0, max = allTableCells.length; i < max; i++) {
     var node = allTableCells[i];
     var currentText = node.childNodes[0].nodeValue;
     if (currentText === "")
       node.style.backgroundColor = "#D3D3D3";
   }
+  if (table != null) {
+    for (var i = 0; i < table.rows.length; i++) {
+      for (var j = 0; j < table.rows[i].cells.length; j++)
+        table.rows[i].cells[j].onclick = function () {
+          getval(this);
+        };
+    }
+  }
+
+  function getval(cel) {
+    var k = cel.innerHTML
+    document.getElementById("date-current").innerHTML = k + " " + months[month] + " " + year;
+
+  }
+
 }
 
 function daysInMonth(iMonth, iYear) {
